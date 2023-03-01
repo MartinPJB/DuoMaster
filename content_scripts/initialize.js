4872d5d2f9bdf6ed97d8d925b3ca4ebed83edd86
Cheeto <martin.bronnimann.pj@gmail.com>
2023-03-01 12:18:45 +0100

    First attempt to insert duomaster settings through content script
    Instead from creating it in the injected script

--- content_scripts/initialize.js
+++ content_scripts/initialize.js
@@ -10,9 +10,28 @@ function injectScript(fileName) {
 }
 
 // Check if the extension is enabled before injecting the script.
-chrome.storage.local.get("DuoMasterEnabled", ({ DuoMasterEnabled }) => {
+chrome.storage.local.get("DuoMasterEnabled", async ({ DuoMasterEnabled }) => {
+	// If enabled
 	if (DuoMasterEnabled) {
-		// injectScript("content_scripts/script.js");
+		// Add the settings
+		const settings = {
+			humanFeel: true,
+			robotSpeed: 500,
+			humanChooseSpeedRange: [500, 900],
+			humanTypeSpeedRange: [50, 300],
+			autoskip: false,
+		};
+
+		// Insert those in every duolingo tab existing in the browser
+		// NOT DONE, CAUSING ERROR
+		const tabs = await chrome.tabs.query({ url: "*://www.duolingo.com/*" });
+		for (const tab of tabs) {
+			chrome.tabs.executeScript(tab.id, {
+				code: `window.duomaster.settings = ${settings}`,
+			});
+		}
+
+		// Inject the script
 		injectScript("content_scripts/injected/script.js");
 	}
 });
