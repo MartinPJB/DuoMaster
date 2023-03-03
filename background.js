5432059e13683af3889654acdff88ce4e040ac5b
COZAX <sushibar2929@gmail.com>
2023-03-03 13:07:26 +0100

    Update background.js

--- background.js
+++ background.js
@@ -1,18 +1,41 @@
+/**
+ * Gets a value from chrome.storage.local asynchronously.
+ * @param {String} key - The key to get from chrome.storage.local.
+ * @returns {Promise} - A promise that resolves with the value.
+ */
+async function chromeStorageGetAsync(key) {
+    return new Promise((resolve, reject) => {
+        chrome.storage.local.get([key], function (result) {
+            if (result[key] === undefined) {
+                reject();
+            } else {
+                resolve(result[key]);
+            }
+        });
+    });
+};
+
 // Function: Handle the installation of the extension and the storage changes
-function handleInstall() {
+async function handleInstall() {
     console.log("DuoMasterEnabled is ready to use 🚀");
+    const options = await chromeStorageGetAsync("DuoMasterSettings");
+    let settings = {
+        humanFeel: false,
+        robotSpeed: 200,
+        humanChooseSpeedRange: [500, 900],
+        humanTypeSpeedRange: [50, 300],
+        autoskip: true,
+        debug: true
+    }
+
+    if (options) settings = options;
     chrome.storage.local.set({
         DuoMasterEnabled: true,
-        DuoMasterSettings: {
-			humanFeel: false,
-			robotSpeed: 200,
-			humanChooseSpeedRange: [500, 900],
-			humanTypeSpeedRange: [50, 300],
-			autoskip: true,
-            debug: true
-		}
+        DuoMasterSettings: settings
     });
 
+    chrome.action.setBadgeText({ text: "ON" });
+    chrome.action.setBadgeBackgroundColor({ color: "#58cc02" });
 }
 
 // Function: Handle the storage changes and update the badge depending on the value
