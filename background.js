cc2cacf214e6f03a1bb787bf5ea23e4a6af71e72
COZAX <sushibar2929@gmail.com>
2023-03-05 22:17:42 +0100

    Small changed background.js

--- background.js
+++ background.js
@@ -1,5 +1,15 @@
 import { chromeStorageGetAsync } from "./include/functions.js";
 
+// Function: Set the badge text and color depending on the value of DuoMasterEnabled
+async function setBadge() {
+    const enabled = await chromeStorageGetAsync("DuoMasterEnabled");
+    let isEnabled = false;
+    if (enabled) isEnabled = enabled;
+
+    chrome.action.setBadgeText({ text: isEnabled ? "ON" : "OFF" });
+    chrome.action.setBadgeBackgroundColor({ color: isEnabled ? "#58cc02" : "#ff4b4b" });
+}
+
 // Function: Handle the installation of the extension and the storage changes
 async function handleInstall() {
     console.log("DuoMasterEnabled is ready to use 🚀");
@@ -20,17 +30,12 @@ async function handleInstall() {
         DuoMasterSettings: settings
     });
 
-    chrome.action.setBadgeText({ text: "ON" });
-    chrome.action.setBadgeBackgroundColor({ color: "#58cc02" });
+    setBadge();
 }
 
 // Function: Handle the storage changes and update the badge depending on the value
 function handleStorage(changes, namespace) {
-    if (changes["DuoMasterEnabled"]) {
-        let isEnabled = Boolean(changes.DuoMasterEnabled.newValue);
-        chrome.action.setBadgeText({ text: isEnabled ? "ON" : "OFF" });
-        chrome.action.setBadgeBackgroundColor({ color: isEnabled ? "#58cc02" : "#ff4b4b" });
-    }
+    setBadge();
 }
 
 // Event: Listener for the extension installation
@@ -38,3 +43,6 @@ chrome.runtime.onInstalled.addListener(handleInstall);
 
 // Event: Listener for storage changes
 chrome.storage.onChanged.addListener(handleStorage);
+
+// Event: Listener for extension loading
+chrome.runtime.onStartup.addListener(setBadge);
